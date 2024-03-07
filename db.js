58212d9307ef763ccb1df77fd0a0e965fcd23500
Cristhian Saavedra <45241687+cristhiansaavedra96@users.noreply.github.com>
2024-03-07 16:34:36 -0300

    Add files via upload

--- db.js
+++ db.js
@@ -1,16 +1,15 @@
 const mysql = require('mysql2/promise');
 
-const mySqlURL = 'mysql://root:a3553fG6EagA5b6fD6613B2d3adaecGc@viaduct.proxy.rlwy.net:11072/railway'
-//const mySqlURL = 'mysql://root:a3553fG6EagA5b6fD6613B2d3adaecGc@mysql.railway.internal:3306/railway'
+//MYSQL_URL2=mysql://root:a3553fG6EagA5b6fD6613B2d3adaecGc@mysql.railway.internal:3306/railway
+//MYSQL_URL=mysql://root:a3553fG6EagA5b6fD6613B2d3adaecGc@viaduct.proxy.rlwy.net:11072/railway
 
 const initiateDb = async () => {
     try {
-        const db = await mysql.createConnection(mySqlURL);
+        const db = await mysql.createConnection(process.env.MYSQL_URL);
         console.time('Connected to database')
         const [results, fields] = await db.query(
             'SELECT * FROM `permisos`'
         );
-        console.log(results)
         console.timeEnd('Connected to database');
     } catch (err) {
         console.error('Error connecting to database: ', err);
